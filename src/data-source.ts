2f0ab0058d1d246c572bebdc43eb352344dd9928
Zheer Ardalan <bestzheer@gmail.com>
2022-06-24 23:00:29 +0300

    adding global database

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,7 +3,7 @@ import { DataSource } from "typeorm"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: "postgres.c5gcbsksozji.us-east-1.rds.amazonaws.com",
     port: 5432,
     username: "postgres",
     password: "clayHP12",
