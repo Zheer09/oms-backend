298f3f7f4b86cbc4a9bd37425457b9610e8c9229
Zheer Ardalan <bestzheer@gmail.com>
2022-07-03 23:55:34 +0300

    Delete AWS Key

--- src/aws-config.ts
+++ src/aws-config.ts
@@ -1,8 +1,8 @@
 
 export const awsConfig = {
 
-accessKeyId: "AKIA3FLUXYFHW3J5IAW6", //TODO: Update
-secretAccessKey: "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",//TODO: Update
+accessKeyId: "", //TODO: Update
+secretAccessKey: "",//TODO: Update
 bucketName: "oms-pic",//TODO: Update
 region: "us-east-1", //TODO: Update
 
